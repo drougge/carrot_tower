46d122c22b0a20e2279f4223f4d0bc86d7ccc4f7
Petter Blomberg <github@petterblomberg.se>
2012-07-13 21:37:37 +0200

    Carrots everywhere!!!!!!!!1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import pygame
 from pygame.locals import *
 from operator import add, sub, mul, div
-from math import hypot
+from math import hypot, atan2, degrees
 
 WIDTH, HEIGHT = 1280, 720 # damn projector
 speed = 60
@@ -15,7 +15,7 @@ _images = {}
 def imgload(name):
 	if name not in _images:
 		img = pygame.image.load(name).convert_alpha()
-		_images[name] = dict([(deg, pygame.transform.rotate(img, deg)) for deg in range(0, 360, 45)])
+		_images[name] = dict([(deg, pygame.transform.rotate(img, deg)) for deg in range(0, 360)])
 	return _images[name]
 
 class Sprite(pygame.sprite.Sprite):
@@ -36,8 +36,7 @@ class Sprite(pygame.sprite.Sprite):
 		c = background.get_at(map(int, map(add, self._pos, m)))
 		if c[0] == 255: return True
 	def _setrot(self):
-		move = tuple([cmp(m, 0) for m in self._move])
-		self._rot = {(1, 0): 0, (0, -1): 90, (-1, 0): 180, (0, 1): 270}[move]
+		self._rot = int(degrees(atan2(*self._move)) - 90) % 360
 		self.image = self._img[self._rot]
 	def update(self):
 		z = map(div, self.image.get_size(), (2, 2))
@@ -86,9 +85,15 @@ class Chainsaw(Sprite):
 		Sprite.__init__(self, "saw_" + colour + "_1.png", x, y, [mx, my])
 
 class Carrot(Sprite):
+	max_speed=8
 	def __init__(self, x, y, target_position):
-		Sprite.__init__(self, "carrot.png", x, y)
-		self.image = self._img[90]
+		posdiff = map(sub, (x, y), target_position)
+		h = hypot(*posdiff)
+		s = h / self.max_speed
+		Sprite.__init__(self, "carrot.png", x, y, map(mul, posdiff, (s, s)))
+
+	def update(self):
+		Sprite.update(self)
 
 def build(what_to_build, position):
 	global money
