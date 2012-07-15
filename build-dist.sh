8afbc10c0316a61cced88e8407155600dbadb939
Carl Drougge <drougg@blue.lundagatan.com>
2012-07-15 11:37:27 +0200

    Add script to build a tarball, and run scripts for windows.

---
+++ build-dist.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rm -f carrot_tower.tar.gz
+cd ..
+tar zcf carrot_tower/carrot_tower.tar.gz carrot_tower/README* carrot_tower/run* carrot_tower/game/*.py carrot_tower/game/*.png carrot_tower/game/*.wav carrot_tower/game/*.jpeg
