1d50acfb8eeaec17d3566b6589e03f514e3705de
Carl Drougge <drougg@blue.lundagatan.com>
2012-07-16 15:43:40 +0200

    Add LICENSE.

--- build-dist.sh
+++ build-dist.sh
@@ -2,4 +2,4 @@
 
 rm -f carrot_tower.tar.gz
 cd ..
-tar zcf carrot_tower/carrot_tower.tar.gz carrot_tower/README* carrot_tower/run* carrot_tower/game/*.py carrot_tower/game/*.png carrot_tower/game/*.wav carrot_tower/game/*.jpeg
+tar zcf carrot_tower/carrot_tower.tar.gz carrot_tower/LICENSE* carrot_tower/README* carrot_tower/run* carrot_tower/game/*.py carrot_tower/game/*.png carrot_tower/game/*.wav carrot_tower/game/*.jpeg
